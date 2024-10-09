3745c5fb4ec97416a5eb940cf2ebc35ffb81d00b
Kitz2024 <kit.pollinger@gmail.com>
2024-10-09 11:49:26 -0700

    11-AM Commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 //Kit Pollinger
-//210-Lab-21 | 
+//210-Lab-21 | Goat Herd Manager 3000
 
-
-
-//
-//210-Demo-Chair-1
 #include <iostream>
 #include <vector>
 #include <iomanip>
@@ -67,80 +63,20 @@ class DoublyLinkedList{
     Node* tail;
 };
 
+int main (){
+    DoublyLinkedList list;
+    list.push_back(Goat("Snior", "Yellow", 6));
+    list.push_back(Goat("Godlike", "Red", 7));
+    list.push_back(Goat("Old", "Red", 2));
+    list.push_back(Goat("Old", "Gold", 6));
+    list.push_back(Goat("Mature", "Mauve", 6));
+    list.push_back(Goat("Teen", "Gold", 10));
+    list.push_back(Goat("Teen", "White", 17));
+    cout << "Forward:" << endl;
+    list.printForward();
 
-
-const int SIZE = 3;
-
-class Chair {
-private:
-    int legs;
-    double * prices;
-public:
-    // constructors
-    Chair() {
-        prices = new double[SIZE];
-        legs = 0;
-        for (int i = 0; i < SIZE; i++)
-            prices[i] = 0;
-    }
-    Chair(int l) {
-        prices = new double[SIZE];
-        legs = l;
-        for (int i = 0; i < SIZE; i++)
-            prices[i] = 0;
-    }
-
-    // setters and getters
-    void setLegs(int l)      { legs = l; }
-    int getLegs()            { return legs; }
-
-    void setPrices(double p1, double p2, double p3) { 
-        prices[0] = p1; prices[1] = p2; prices[2] = p3; 
-    }
-
-    double getAveragePrices() {
-        double sum = 0;
-        for (int i = 0; i < SIZE; i++)
-            sum += prices[i];
-        return sum / SIZE;
-    }
-
-    void print() {
-        cout << "CHAIR DATA - legs: " << legs << endl;
-        cout << "Price history: " ;
-        for (int i = 0; i < SIZE; i++)
-            cout << prices[i] << " ";
-        cout << endl << "Historical avg price: " << getAveragePrices();
-        cout << endl << endl;
-    }
-};
-
-int main() {
-    cout << fixed << setprecision(2);
-
-    //creating pointer to first chair object
-    Chair *chairPtr = new Chair;
-    chairPtr->setLegs(4);
-    chairPtr->setPrices(121.21, 232.32, 414.14);
-    chairPtr->print();
-
-    //creating dynamic chair object with constructor
-    Chair *livingChair = new Chair(3);
-    livingChair->setPrices(525.25, 434.34, 252.52);
-    livingChair->print();
-    delete livingChair;
-    livingChair = nullptr;
-
-    //creating dynamic array of chair objects
-    Chair *collection = new Chair[SIZE];
-    collection[0].setLegs(4);
-    collection[0].setPrices(441.41, 552.52, 663.63);
-    collection[1].setLegs(4);
-    collection[1].setPrices(484.84, 959.59, 868.68);
-    collection[2].setLegs(4);
-    collection[2].setPrices(626.26, 515.15, 757.57);
-    for (int i = 0; i < SIZE; i++)
-        collection[i].print();
+    cout << "\nBackward:" << endl;
+    list.printBackward();
     
     return 0;
 }
